087f1a189360e377058ae3337d01e1b2b296b0d2
bp-riv <biagio.paparella@rivacon.com>
2024-04-25 11:01:22 +0200

    ok

--- test_c.c
+++ test_c.c
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 #include<stdio.h>
 
 int main (){
 	printf("hello, world - version MAIN -- \n");
 	return 0;
 }
-=======
-#include<stdio.h>
-
-int main (){
-	return 0;
-}
->>>>>>> alternative
